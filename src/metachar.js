96648df4739a9897a5f3e8861fc8c440f9048a19
MrAdhit <47937950+MrAdhit@users.noreply.github.com>
2021-05-02 08:35:18 +0700

    Add jsdoc
    Added jsdoc
    Minify some code

--- src/metachar.js
+++ src/metachar.js
@@ -3,15 +3,23 @@ class MetaChar {
 
     }
 
+    /**
+     * Stringify metacharacter
+     * @param {string} value String with metacharacter
+     * @returns {string}
+     */
     stringify(value){
         value = {value};
         return JSON.stringify(value).replace("{\"value\":\"", "").replace("\"}", "");
     }
 
+    /**
+     * Parse stringified metacharacter
+     * @param {string} value Stringified string with metacharacter
+     * @returns {string}
+     */
     parse(value){
-        value = {
-            value: value.replace(/\\\\/g, "\\").replace(/\\\\/g, "\\").replace("{\"value\":\"", "").replace("\"}", "")
-        }
+        value = {value}
         return JSON.parse(JSON.stringify(value).replace(/\\\\/g, "\\").replace(/\\\\/g, "\\")).value
     }
 }
